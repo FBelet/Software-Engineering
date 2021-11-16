d79ae67e79e6938d18cdc3d37fcf31052ccb98e4
Fabienne <fabienne_belet@sunrise.ch>
2021-11-16 22:13:58 +0100

    Added additional line

---
+++ Universität St.Gallen/Flumenbaum, BereniceJudith - Software Engineering/Project.py
@@ -0,0 +1,28 @@
+
+"""
+
+## SOFTWARE ENGINEERING ##
+      ### GROUP 3 ###
+
+Berenice Flumenbaum & Fabienne Belet
+
+
+"""
+
+# import modules
+
+import sys
+import pandas as pd
+import numpy as np
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+# set working directory
+PATH = 'C:/Users/fabie/Universität St.Gallen/Flumenbaum, BereniceJudith - Software Engineering'
+sys.path.append(PATH)
+
+# load functions
+import Project_Functions as pc
+
+# start project
