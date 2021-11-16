a51d7380bccf7ad4d532fa13675faf59e4c237c1
Fabienne <fabienne_belet@sunrise.ch>
2021-11-16 21:22:21 +0100

    Set up the Project Code

--- Universität St.Gallen/Flumenbaum, BereniceJudith - Software Engineering/Project.py
+++ Universität St.Gallen/Flumenbaum, BereniceJudith - Software Engineering/Project.py
@@ -9,3 +9,17 @@ Berenice Flumenbaum & Fabienne Belet
 
 """
 
+# import modules here
+import sys
+import pandas as pd
+import numpy as np
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+# set working directory
+PATH = 'C:/Users/fabie/Universität St.Gallen/Flumenbaum, BereniceJudith - Software Engineering'
+sys.path.append(PATH)
+
+# load functions
+import Project_Functions as pc
