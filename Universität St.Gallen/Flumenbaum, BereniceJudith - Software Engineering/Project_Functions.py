a45e6510378bae40527a3dcde995df7062020122
FBelet <94165736+FBelet@users.noreply.github.com>
2021-11-26 12:03:07 +0100

    Added modules to code

--- Universität St.Gallen/Flumenbaum, BereniceJudith - Software Engineering/Project_Functions.py
+++ Universität St.Gallen/Flumenbaum, BereniceJudith - Software Engineering/Project_Functions.py
@@ -10,6 +10,12 @@ Berenice Flumenbaum & Fabienne Belet
 """
 
 # import modules
+import sys
+import pandas as pd
+import numpy as np
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 
 
